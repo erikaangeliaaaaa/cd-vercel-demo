2e37cd373526d0dbe5d57db9af97713bffabdc79
angelia3443 <erikaangelia2005@gmail.com>
2026-03-15 23:05:28 +0700

    fix vercel config

--- api/index.py
+++ api/index.py
@@ -1,10 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "<h1>Hello from Python on Vercel 🚀</h1>"
-
 def handler(request):
-    return app(request.environ, lambda *args: None)
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/html"},
+        "body": "<h1>Hello from Python on Vercel 🚀</h1>"
+    }
