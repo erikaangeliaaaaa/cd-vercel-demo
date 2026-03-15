f0d35cce79239c8438f72cf762715c28fa0069e4
angelia3443 <erikaangelia2005@gmail.com>
2026-03-15 23:14:27 +0700

    now

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/html"},
-        "body": "<h1>Hello from Python on Vercel 🚀</h1>"
-    }
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Python on Vercel 🚀"
